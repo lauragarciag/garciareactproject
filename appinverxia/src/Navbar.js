c42c028c9c2caec1f3167b1f98bc926525e25870
Laura Garcia <laurgar@gmail.com>
2022-07-16 21:58:08 -0400

    desafio2

---
+++ appinverxia/src/Navbar.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">Inverxia - Educación Financiera</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Home</a>
+        <a href="/about">Quién soy</a>
+        <a href="/excel">Cursos Excel</a>
+        <a href="/finanzas">Cursos Finanzas</a>
+        <a href="/inversion">Cursos Inversion</a>
+        <a href="/contact">Contacto</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
