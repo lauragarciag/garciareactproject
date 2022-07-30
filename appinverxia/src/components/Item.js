8bc06f6c367f9f50a1b2b8e25b006b2efb10a829
Laura Garcia <laurgar@gmail.com>
2022-07-30 10:39:10 -0400

    desafio3y4

--- appinverxia/src/components/Item.js
+++ appinverxia/src/components/Item.js
@@ -3,14 +3,15 @@ import ItemCount from './ItemCount';
 import './Item.css';
 import { getProductsInverxia } from '../asyncMock';
 
-const Item = ({ name, thumbnail, price, id, stock }) => {
+const Item = ({products}) => {
+  const { name, img, price, id, stock } = products;
   const onAdd = (qty) => {
     alert(`Has agregado ${qty} el curso`);
   };
 
   return (
     <article className="product-card">
-      <img className="product-card__image" src={thumbnail} alt="" />
+      <img className="product-card__image" src={img} alt="" />
 
       <h3 className="product-card__name">{name}</h3>
       <span className="product-card__name">${price}</span>
