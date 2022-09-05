0f9593f690b35dd7d7d2264edc594f2f12e1ddc3
Laura Garcia <laurgar@gmail.com>
2022-09-05 18:44:27 -0300

    revision franco

--- appinverxia/src/components/ItemListContainer.js
+++ appinverxia/src/components/ItemListContainer.js
@@ -2,23 +2,50 @@ import './ItemListContainer.css'
 import { useState, useEffect } from 'react'
 import ItemList from './ItemList'
 import { useParams } from 'react-router-dom' 
-import { db, } from '../services/firebase'
+import { getDocs, collection, query, where } from 'firebase/firestore'
+import { db } from '../services/firebase'
 
 
 const ItemListContainer = ({ greeting }) => {
-      const { categoryId } = useParams()
-      const getProductosFromFirestore = () => db(categoryId)
-   
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const { categoryId } = useParams()
+
+  useEffect(() => {
+      setLoading(true)
+
+      const collectionRef = !categoryId 
+          ? collection(db, 'products')
+          : query(collection(db, 'products'), where('category', '==', categoryId))
+
+      getDocs(collectionRef).then(response => {
+          const productsAdapted = response.docs.map(doc => {
+              const data = doc.data()
+              return { id: doc.id, ...data}
+          })
+          setProducts(productsAdapted)
+      }).catch(error => {
+          console.log(error)
+      }).finally(() => {
+          setLoading(false)
+      })
+
+  
+  }, [categoryId])
 
 
-    return(
-      <>
-        <h1>{greeting}</h1>
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
-      
-        </div>
-      </>
-    )
+  if(loading) {
+      return <h1>Cargando productos...</h1>
+  }
+
+
+
+  return (
+      <div onClick={() => console.log('click en itemlistcontainer')}>
+          <h1>{`${greeting} ${categoryId || ''}`}</h1>
+          <ItemList products={products} />
+      </div>
+  )
 }
 
 export default ItemListContainer
